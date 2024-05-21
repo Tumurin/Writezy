bd6422e40175fd7b63c5d11a39f50bbcf61a79b7
Tumurin <kuanminzhao@gmail.com>
2024-05-22 01:06:43 +0800

    [feature]完善 Nav ：添加 Icon Button 元件

--- components/Nav/index.vue
+++ components/Nav/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="container">
+  <div class="container-lg container-fluid">
     <!-- 導航 -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <UILogo></UILogo>
-        消息 通知
-        <UIAvatar></UIAvatar>
+      <div class="row w-100">
+        <div class="col-lg-3 col-2">
+          <UILogo></UILogo>
+        </div>
+        <div class="col-lg-6 col-8">
+          <SearchInput></SearchInput>
+        </div>
+        <div class="col-lg-3 col-2 d-flex justify-content-end">
+          <ButtonFlatBell class="d-none d-lg-block"></ButtonFlatBell>
+          <ButtonFlatNotification class="d-none d-lg-block"></ButtonFlatNotification>
+          <UIAvatar class="d-none d-lg-block" userId="abcd1234"></UIAvatar>
+          <ButtonFilledBergurMenu class="d-lg-none"></ButtonFilledBergurMenu>
+        </div>
       </div>
     </nav>
   </div>
