24e7a5fe13a3dc924e00f2918fbf1bdd09fb7623
Tumurin <kuanminzhao@gmail.com>
2024-06-18 02:17:10 +0800

    [feature] 搜尋框功能完善：修正邏輯，版面調整

--- components/Search/Input.vue
+++ components/Search/Input.vue
@@ -1,102 +1,150 @@
 <template>
-  <div class="container d-flex ">
-    <form class="form-signin wrapper m-auto" autocomplete="off">
-      <div class="input-group-clear" style="position:relative">
-        <input type="text" id="username" v-model="inputValue" class="form-control" 
-          @focus="showHistory = true" @blur="hideDropdown" @input="addSearchQuery" required autofocus>
-          <div v-show="!inputValue">
-            <span class="input-search">
-              <Icon name="material-symbols-light:search" /> 
-            </span>
-            <span class="input-placeholder">搜尋</span>
+  <div class="container d-flex">
+    <form
+      class="form-signin wrapper m-auto"
+      @submit.prevent="addSearchQuery"
+      autocomplete="off"
+    >
+      <div class="input-group-clear" style="position: relative">
+        <span class="input-search">
+          <Icon name="material-symbols-light:search" />
+        </span>
+        <input
+          type="text"
+          v-model="inputValue"
+          class="form-control ps-5"
+          @keydown="onKeyDown"
+          @focus="showHistory = true"
+          @blur="hideHistory"
+          placeholder="搜尋"
+          required
+          autofocus
+        />
+
+        <span class="input-clear" v-show="inputValue" @click="clearInput">
+          <Icon name="ic:outline-clear" />
+        </span>
+
+        <!-- 搜尋展開視窗 -->
+        <div v-if="showHistory" class="dropdown-menu show wrapper px-2 mt-2">
+          <div class="d-flex justify-content-between">
+            <span>近期搜尋</span>
           </div>
+          <div class="horizontal-line-purple my-2"></div>
+          <div>
+            <button
+              type="button"
+              class="dropdown-item"
+              v-for="(item, index) in searchHistory"
+              :key="index"
+              :class="{ active: index === selectedIndex }"
+              @mousedown.prevent="selectHistoryItem(index)"
+            >
+              <div class="d-flex justify-content-between">
+                <span>
+                  <Icon class="me-3" name="mdi:clock-outline" />
+                  <span>{{ item }}</span>
+                </span>
+                <span type="button" @click="deleteHistory(index)">
+                  <Icon name="ic:outline-clear" />
+                </span>
+              </div>
 
-          <span class="input-clear" v-show="inputValue" @click="clearInput">
-            <Icon name="ic:outline-clear"/>
-          </span>
-
-          <!-- 搜尋展開視窗 -->
-          <div v-if="showHistory" class="dropdown-menu show wrapper px-2">
-            <div class="d-flex justify-content-between">
-              <span>近期搜尋</span>
-              <button class="btn btn-link p-0" @click="toggleEditMode">{{ editMode ? '完成' : '編輯' }}</button>
-            </div>
-            <div class="horizontal-line-purple my-2"></div>
-            <div>
-              <button
-                class="dropdown-item"
-                v-for="(item, index) in searchHistory"
-                :key="index"
-              >
-                <div class="d-flex justify-content-between">
-                  <span>
-                    <Icon class="me-3" name="mdi:clock-outline" />
-                    <span @click="selectHistoryItem(item)">{{ item }}</span>
-                  </span>
-
-                  <span v-if="editMode" type="button" @click="deleteHistory(index)">
-                    <Icon name="ic:outline-clear"/>
-                  </span>
-                </div>
-                
-
-                <!-- <div>
+              <!-- <div>
                   <ItemsDeleteFriendNoIcon></ItemsDeleteFriendNoIcon>
                 </div> -->
-              </button>
-              
-            </div>
+            </button>
           </div>
-
+        </div>
       </div>
     </form>
   </div>
 </template>
 
 <script setup>
-const inputValue = ref('')
+const router = useRouter();
+
+const inputValue = ref("");
 const clearInput = () => {
-  inputValue.value = ''
+  inputValue.value = "";
 };
 
 // 搜尋展開視窗
-const searchHistory = ref([])
-const showHistory = ref(false)
-const editMode = ref(false)
+const searchHistory = ref([]);
+const showHistory = ref(false);
+const selectedIndex = ref(-1);
+
+onMounted(() => {
+  loadSearchHistory();
+});
 
 const loadSearchHistory = () => {
-  if (typeof localStorage !== 'undefined') {
-    const history = localStorage.getItem('searchHistory')
+  if (typeof localStorage !== "undefined") {
+    const history = localStorage.getItem("searchHistory");
     if (history) {
-      searchHistory.value = JSON.parse(history)
+      searchHistory.value = JSON.parse(history);
     }
   }
-}
-
-const toggleEditMode = () => {
-  editMode.value = !editMode.value
-}
+};
 
 const addSearchQuery = () => {
   if (inputValue.value && !searchHistory.value.includes(inputValue.value)) {
-    searchHistory.value.unshift(inputValue.value)
-    localStorage.setItem('searchHistory', JSON.stringify(searchHistory.value))
+    searchHistory.value.unshift(inputValue.value);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory.value));
   }
-}
+  navigateToSearch();
+};
 
 const deleteHistory = (index) => {
-  searchHistory.value.splice(index, 1)
-  localStorage.setItem('searchHistory', JSON.stringify(searchHistory.value))
-}
+  searchHistory.value.splice(index, 1);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory.value));
+};
 
-const selectHistoryItem = (item) => {
-  inputValue.value = item
-  // 可以在這裡觸發進一步的搜尋行為
-}
+const selectHistoryItem = (index) => {
+  inputValue.value = searchHistory.value[index];
+  selectedIndex.value = index;
+  hideHistory();
+};
 
-onMounted(() => {
-  loadSearchHistory()
-})
+const hideHistory = () => {
+  setTimeout(() => {
+    showHistory.value = false;
+  }, 200);
+};
+
+const navigateToSearch = () => {
+  router.push({ path: "/search", query: { q: inputValue.value } });
+};
+
+const onKeyDown = (event) => {
+  if (!showHistory.value) return;
+
+  switch (event.key) {
+    case "ArrowDown":
+      if (selectedIndex.value < searchHistory.value.length - 1) {
+        selectedIndex.value++;
+      }
+      break;
+    case "ArrowUp":
+      if (selectedIndex.value > 0) {
+        selectedIndex.value--;
+      }
+      break;
+    case "Enter":
+      if (selectedIndex.value >= 0) {
+        selectHistoryItem(selectedIndex.value);
+      }
+      break;
+    case "Tab":
+      if (selectedIndex.value >= 0) {
+        event.preventDefault();
+        selectHistoryItem(selectedIndex.value);
+      }
+      break;
+    default:
+      break;
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -105,7 +153,7 @@ onMounted(() => {
   width: 100%;
 }
 
-input:disabled  {
+input:disabled {
   border-color: $gray-200;
   background-color: $gray-000;
 }
@@ -134,7 +182,7 @@ input:disabled  {
   font-size: 20px;
   color: $black;
 }
-  
+
 .input-clear {
   position: absolute;
   right: 10px;
@@ -152,5 +200,10 @@ input:disabled  {
   width: 100%;
   height: 1px;
   border-top: solid 1px #cdaeef;
+}
+
+.dropdown-item.active {
+  background-color: #007bff;
+  color: white;
 }
 </style>
