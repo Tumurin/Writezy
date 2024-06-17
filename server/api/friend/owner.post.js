9c053393cb436f1504e1813fddc85d921a92db37
Tumurin <kuanminzhao@gmail.com>
2024-06-17 09:23:21 +0800

    [fix] 移除未使用到的引入

--- server/api/friend/owner.post.js
+++ server/api/friend/owner.post.js
@@ -1,20 +1,19 @@
-import { useSSRContext } from "vue";
 import { getUser } from "~/server/db/users";
 
-export default defineEventHandler(async(event)=>{
-    try{
+export default defineEventHandler(async (event) => {
+    try {
         const body = await readBody(event);
         const user = await getUser(body)
         return {
-            code:200,
-            data:user,
-            message:"成功登入"
+            code: 200,
+            data: user,
+            message: "成功登入"
         }
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return {
-            code:400,
-            message:err
+            code: 400,
+            message: err
         }
     }
 })
